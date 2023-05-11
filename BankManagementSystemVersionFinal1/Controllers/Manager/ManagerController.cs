fe49b47e982a8a0a4fd71fcd2a8a7e1f2dcd8343
omar <romdaneo@gmail.com>
2023-05-11 17:04:45 +0100

    oo

---
+++ BankManagementSystemVersionFinal1/Controllers/Manager/ManagerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankManagementSystemVersionFinal1.Controllers.Manager
+{
+
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
